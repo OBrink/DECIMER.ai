7ed9ac45dc493310dd3e97de351c17dc7096f843
OBrink <brinkhaus@gmail.com>
2022-04-28 09:49:47 +0000

    force https routes

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,17 @@ use App\Http\Controllers\StoutController;
 use App\Http\Controllers\SocketServerInitController;
 use App\Http\Controllers\ProblemReportController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/', function () {
     return view('index');
 })->name('home');
@@ -56,3 +67,4 @@ Route::post('stout-iupac', [StoutController::class, 'StoutPost'])->name('stout.i
 Route::get('problem-report', [ProblemReportController::class, 'ProblemReport'])->name('problem.report');
 Route::post('problem-report', [ProblemReportController::class, 'ProblemReportPost'])->name('problem.report.post');
 
+URL::forceScheme('https');
