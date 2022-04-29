5c6217617600cbc9e3b9c20c842d667d0f622e87
Otto Brinkhaus <brinkhaus@gmail.com>
2022-04-29 10:33:33 +0200

    add some to first section padding

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <img src="DECIMER.gif" alt="DECIMER Logo" id="decimer_logo_gif" style="display: none;" />
         <img src="DECIMER.png" alt="DECIMER Logo" id="decimer_logo" style="display: none;" />
         <img src="loading_icon.gif" alt="Loading icon" class="mx-auto" id="loading_icon" style="display: none;" />
-        <div class="pt-5">
+        <div class="p-5">
             <!-- DECIMER LOGO (Animated gif is only shown the first time we are sent to index view) -->
             @if (!Session::get('img_paths'))
                 <script>
