ba9c1b9235d979c3daa719179a2f71a733ef10c0
Otto Brinkhaus <brinkhaus@gmail.com>
2022-04-29 16:05:22 +0200

    Stop cached website version from causing problems

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,6 +4,9 @@
 	<meta charset="UTF-8">
 	<meta name="index" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie-edge">
+	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate"/>
+	<meta http-equiv="Pragma" content="no-cache"/>
+	<meta http-equiv="Expires" content="0"/>
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 	<link rel="stylesheet" href="{{ asset('css/tailwind.min.css') }}">
 	<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
