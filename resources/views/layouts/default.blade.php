74bb25883a59cf0a73a82f955e8beb75d0d17430
Otto Brinkhaus <brinkhaus@gmail.com>
2023-03-02 17:08:36 +0100

    add matomo script

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -23,6 +23,19 @@
 
 		gtag('config', 'G-VKSWMKC79R');
 	</script>
+	<script>
+		var _paq = window._paq = window._paq || [];
+		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function() {
+			var u="//matomo.nmrxiv.org/";
+			_paq.push(['setTrackerUrl', u+'matomo.php']);
+			_paq.push(['setSiteId', '2']);
+			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+			g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+		})();
+	</script>
 </head>
 
 
